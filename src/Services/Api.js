adeb2247941b3110e4bd76bf8ddc4fa29bceeab9
Alexsandro Martins <alexsandromart2002@protonmail.com>
2022-02-14 18:25:52 -0300

    Att

--- src/Services/Api.js
+++ src/Services/Api.js
@@ -4,14 +4,4 @@ const api = axios.create({
   baseURL: "https://admin-portal-over.herokuapp.com",
 });
 
-api.interceptors.request.use(async (config) => {
-  let response = localStorage.getItem("over_token");
-  var token = `Bearer ${response}`;
-
-  if (response) {
-    config.headers.Authorization = token;
-  }
-  return config;
-});
-
 export default api;
